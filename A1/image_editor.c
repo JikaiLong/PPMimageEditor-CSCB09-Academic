5d0de416bc891f06568dab8cab8ab409a09f5016
longjika@UTORONTO.CA <longjika@UTORONTO.CA@08fbd934-1aca-4df4-a1c4-682f1001a59e>
2017-05-19 22:33:30 +0000

    First Step Finished, no error found

---
+++ A1/image_editor.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Include the function prototypes of all those functions implemented in image_processing.c.
+ * Do not remove or modify them in any way.
+ */
+
+void remove_red();
+void convert_to_black_and_white();
+void instagram_square();
+
+int main(int argc, char *argv[]) {
+        /* Task 1 needs to be completed here: Make sure that the command line arguments are correct. In case an error exists,
+         * print the error message and terminate the program. Otherwise, invoke the corresponding
+         * image processing function. */
+	/* If the number of command arguments is not equal to 1, exit the program with return 1 to indicate an error*/  
+	if(argc != 2){
+		fprintf(stderr, "%s", "Error: Expecting one command-line argument, which needs to be either 1, 2, or 3.\n");
+		exit(1); 
+	}
+	/* If the number of command arguments is equal to 1, do the following*/ 
+	else{
+	/* use variable to store command argument*/
+		char* arg = argv[1];
+		/*if the command argument is not 1, 2 or 3, exit the program with return 1 to indicate an error*/
+		if(strcmp(arg, "1") == 0 || strcmp(arg, "2") == 0 || strcmp(arg, "3") == 0){
+			printf("ok, this is fine");
+		}
+		else{
+			fprintf(stderr, "%s", "Error: Expecting one command-line argument, which needs to be either 1, 2, or 3.\n");
+			exit(-2);
+		}
+        
+
+}
+return 0;
+}
